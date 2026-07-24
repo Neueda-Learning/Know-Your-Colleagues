a21e019883cffd81e3e943410f044635cd70f11f
QSSH <982840585@qq.com>
2026-07-24 14:23:06 +0800

    Initial commit
    # Conflicts:
    #	.gitignore

---
+++ src/main/java/com/example/knowyourcolleagues/KnowYourColleaguesApplication.java
@@ -0,0 +1,13 @@
+package com.example.knowyourcolleagues;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowYourColleaguesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnowYourColleaguesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/knowyourcolleagues/KnowYourColleaguesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.knowyourcolleagues;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowYourColleaguesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
